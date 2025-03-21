fd31ee60757b20f42c16a5fb629170b8a165518e
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-03-21 16:15:48 +0000

    Fixed black pt.2

--- area_per_deca.py
+++ area_per_deca.py
@@ -8,17 +8,22 @@ import math
 
 
 def main():
+
     # Code first greets user
     print("Hello! Welcome to Jayden's Decagon calculator")
+
     # This give the user input for the length
     side = float(input("What is the side length  of the decagon? (cm)"))
+
     # This makes the code stop if the User picks a negative number or zero.
     if side <= 0:
         print("Please pick a positive number!")
+
+        # The Code proceeds if positive
     else:
-        area = 5 / 2 * (side**2) * math.sqrt(5 + 2 * math.sqrt(5))
+        area = 5 / 2 * (side ** 2) * math.sqrt(5 + 2 * math.sqrt(5))
         perimeter = 10 * side
-        # This part of the code calculates the Area and Perimeter with the given input
+        # This part of the code calculates perimeter and area.
         print("Area is {:.2f}cm^2".format(area))
         print("Perimeter is {:.2f}cm".format(perimeter))
 
